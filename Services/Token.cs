5b15984c980ecd5d15566002dfced06f34131a78
Andrew Smith <andrew.smith@wwinc.com>
2022-01-17 13:25:13 -0600

    Formatted Token.cs

--- Services/Token.cs
+++ Services/Token.cs
@@ -10,12 +10,12 @@ using Microsoft.IdentityModel.Tokens;
 namespace FirstAPI.Services; 
 
 public class Token {
-    private Repository _repository;
+    private Repository    _repository;
     private IConfiguration _config;
 
     public Token(IConfiguration config) {
         _repository = new Repository(config);
-        _config = config;
+        _config      = config;
     }
     
     public async Task<string> GenerateToken(string snn) {
@@ -25,19 +25,24 @@ public class Token {
             
         // Obtains all the bank accounts the user owns
         List<BankAccount>? userBankAccounts = await _repository.GetBankAccountsBySnn(snn);
-        List<int> bankIdOwned = new();
-        List<Claim> claims = new();
+        List<int>          bankIdOwned      = new();
+        List<Claim>        claims           = new();
             
         // Adds the account ids of all bank accounts the user owns to the token claims
-        if (userBankAccounts != null && userBankAccounts.Count > 0) {
+        if (
+            userBankAccounts       != null && 
+            userBankAccounts.Count > 0
+        ) {
             foreach (BankAccount account in userBankAccounts) {
                 bankIdOwned.Add(account.Accountid);
             }
 
             string bankIdOwnedString = "";
+            
             foreach (int id in bankIdOwned) {
                 bankIdOwnedString += "" + id + " ";
             }
+            
             bankIdOwnedString = bankIdOwnedString.Substring(0, bankIdOwnedString.Length - 1);
 
             claims.Add(new Claim("BankAccountsOwned", bankIdOwnedString));
@@ -45,11 +50,12 @@ public class Token {
             
         // Generates the token and returns it
         var token = new JwtSecurityToken(
-            issuer: _config["Jwt:Issuer"],
-            audience: _config["Jwt:Issuer"],
-            claims: claims,
-            expires: DateTime.Now.AddHours(4),
-            signingCredentials: credentials);
+            issuer:             _config["Jwt:Issuer"],
+            audience:           _config["Jwt:Issuer"],
+            claims:             claims,
+            expires:            DateTime.Now.AddHours(4),
+            signingCredentials: credentials
+        );
 
         return new JwtSecurityTokenHandler().WriteToken(token);
     }
