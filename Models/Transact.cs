26705d081ea374e7008caca769c1260b9c8cc2fe
Andrew Smith <andrew.smith@wwinc.com>
2022-01-17 12:09:37 -0600

    Formatted models using vertical alignment and collapsed method/constructor parameters.

--- Models/Transact.cs
+++ Models/Transact.cs
@@ -1,20 +1,27 @@
 namespace demoWebAPI.models; 
 
 public class Transact {
-    public int Acntid { get; set; }
-    public string Act { get; set; }
-    public double Amount { get; set; }
+    public int    Acntid  { get; set; }
+    public string Act     { get; set; }
+    public double Amount  { get; set; }
     public string Account { get; set; }
-    public double Newbal { get; set; }
-    public String TDate { get; set; }
+    public double Newbal  { get; set; }
+    public String TDate   { get; set; }
 
-    public Transact(int acntid, string act, double amount, string account, double newbal, string tdate) {
-        this.Acntid = acntid;
-        this.Act = act;
-        this.Amount = amount;
+    public Transact(
+        int    acntid, 
+        string act, 
+        double amount, 
+        string account, 
+        double newbal, 
+        string tdate
+    ) {
+        this.Acntid  = acntid;
+        this.Act     = act;
+        this.Amount  = amount;
         this.Account = account;
-        this.Newbal = newbal;
-        this.TDate = tdate;
+        this.Newbal  = newbal;
+        this.TDate   = tdate;
     }
     
 
--- Models/UserAccount.cs
+++ Models/UserAccount.cs
@@ -1,13 +1,13 @@
 namespace demoWebAPI.models;
 
 public class UserAccount {
-    public string Name { get; set; }
-    public string Username { get; set; }
-    public string Pass { get; set; }
+    public string Name      { get; set; }
+    public string Username  { get; set; }
+    public string Pass      { get; set; }
     public string Birthdate { get; set; }
-    public string Addr { get; set; }
-    public string Phone { get; set; }
-    public string Snn { get; set; }
+    public string Addr      { get; set; }
+    public string Phone     { get; set; }
+    public string Snn       { get; set; }
 
     public override string ToString() {
         return $"({Name}, {Username}, {Pass}, {Birthdate}, " +
